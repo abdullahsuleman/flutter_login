b51bb7f3e69f72d216ffc91d54d75d57993cb91f
abdullahsuleman1 <abdullah@bigosoft.co.uk>
2021-04-07 16:36:52 +0500

    image update 0.3

--- lib/flutter_login.dart
+++ lib/flutter_login.dart
@@ -620,6 +620,10 @@ class _FlutterLoginState extends State<FlutterLogin>
               begin: Alignment.topCenter,
               end: Alignment.center,
             ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Image(image: AssetImage('assets/images/login_footer_car.png'), width: MediaQuery.of(context).size.width*0.7),
+            ),
             SingleChildScrollView(
               child: Theme(
                 data: theme,
